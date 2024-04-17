5020e374e0cba1217c2ba156ec207f61ce34d569
paulbg <paulbg@hey.com>
2024-04-17 13:20:43 +0300

    feat: implement the FAQ section of the index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
 import { SolidText } from "@/components/routeRelated/index/SolidText/SolidText";
+import { NavigationArrow } from "../components/routeRelated/index/NavigationArrow";
+import { Faq } from "@/components/routeRelated/Faq/Faq";
 
 export default function Home() {
   return (
-    <main>
-      <SolidText />
-    </main>
+    <>
+      <section className="h-screen flex-grow grid place-items-center">
+        <SolidText />
+        <NavigationArrow />
+      </section>
+      <section className="h-screen grid justify-center pt-24">
+        <Faq />
+      </section>
+    </>
   );
 }
